ee63ab8374774ce6e9d846784f6c97d372c14c26
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-01-06 23:50:55 -0300

    .

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-const connection = new Sequelize(process.env.DB_URL)
+const connection = new Sequelize(process.env.DB_URL,{dialect:"mariadb"})
 // const connection = new sequelize(
 //   process.env.DB_NAME,
 //   process.env.DB_LOGIN,
