0eef128e61fdcd708c622a386d8268dab1d5eca5
Gustavo Monteiro <gussmonteiro@gmail.com>
2022-01-06 23:55:22 -0300

    ,

--- database/db.js
+++ database/db.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 // const connection = new Sequelize(process.env.DB_URL, { dialect: "mariadb" });
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_LOGIN, process.env.DB_PASS, {
   dialect: "mariadb",
-  dialectOptions: { connectTimeout: 1000 }, // mariadb connector option
+  dialectOptions: { connectTimeout: 1000 },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
 });
 // const connection = new sequelize(
 //   process.env.DB_NAME,
